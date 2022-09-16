707e0b8e3c25419aad407cd44f6426cad9ce6fd6
Rafael Borges <rafaelborges.av@gmail.com>
2022-09-15 21:37:25 -0300

    primeiro commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <TheHeader />
+</template>
+
+<script>
+import TheHeader from '~/components/TheHeader.vue';
+export default {
+  name: 'IndexPage',
+  components: { TheHeader },
+};
+</script>
