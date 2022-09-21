4e932e75a5247d490fc265c41353512b6cb10859
Rafael Borges <rafaelborges.av@gmail.com>
2022-09-21 16:59:53 -0300

    instalando vite

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,7 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    'nuxt-vite',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
