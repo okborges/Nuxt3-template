d512baed69341b5957eb715e766fcbed2c004749
okborges <rafaelborges.av@gmail.com>
2023-06-06 15:31:34 -0300

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,18 @@
 module.exports = {
-  extends: [
-    // add more generic rulesets here, such as:
-    // 'eslint:recommended',
-    'plugin:vue/vue3-recommended'
-    // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
-  ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-strongly-recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['vue'],
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    quotes: ['error', 'single'],
+    indent: ['error', 2]
   }
 };
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
 import TheHeader from '@/components/TheHeader.vue';
 </script>
