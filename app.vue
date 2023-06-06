537614879e11c8b9092b3cd436e24595f22524dc
okborges <rafaelborges.av@gmail.com>
2023-06-06 14:15:58 -0300

    atualizando nuxt

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div>
+        <section>teita</section>
+        <NuxtWelcome />
+    </div>
 </template>
+
+<style lang="scss" scoped>
+section {
+    height: 300px;
+    width: 300px;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/style.scss'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+    devtools: { enabled: true },
+    css: ['~/assets/style/main.scss'],
 });
