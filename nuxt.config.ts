76251c1c6d83daab8529dab92b92576533a84efe
okborges <rafaelborges.av@gmail.com>
2023-06-06 14:34:22 -0300

    feature: css reset and eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    css: ['~/assets/style/main.scss'],
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'pt-br'
+      },
+      link: [],
+      meta: [
+        {
+          charset: 'utf-8'
+        },
+        {
+          name: 'description',
+          content: 'descrição'
+        },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1'
+        }
+      ],
+      title: 'Titulo'
+    }
+  },
+
+  devtools: { enabled: true },
+
+  css: ['~/assets/style/main.scss']
 });
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheHeader from '@/components/TheHeader.vue';
+</script>
+
+<template>
+  <div>
+    <TheHeader />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
