c12ba42ee7e32904e01b70889b3a7e3b8082ddf6
okborges <rafaelborges.av@gmail.com>
2023-06-06 18:13:21 -0300

    autosave

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,10 +2,7 @@
 
 <template>
   <header>
-    <a
-      href="/"
-      class="logo"
-    >CSS Nav</a>
+    <a href="/" class="logo">CSS Nav</a>
 
     <ul>
       <li><a href="#work">Our Work</a></li>
@@ -18,24 +15,24 @@
 
 <style lang="scss" scoped>
 header {
-    display: flex;
-    justify-content: space-between;
-    padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
 
-    ul {
-        display: flex;
-        gap: 20px;
-    }
+  ul {
+    display: flex;
+    gap: 20px;
+  }
 }
 
 @media (max-width: 500px) {
-    header {
-        flex-direction: column;
-        align-items: center;
+  header {
+    flex-direction: column;
+    align-items: center;
 
-        ul {
-            margin-top: 20px;
-        }
+    ul {
+      margin-top: 20px;
     }
+  }
 }
 </style>
